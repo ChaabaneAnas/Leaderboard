3b80ad46fff231933de411dcdb0ffbac5971db27
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-08-26 11:51:48 +0100

    Hit The API required changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import './style.css';
 import APIhandel from './modules/APIhandler';
-import DsisplayScores from './modules/Ui';
+import { DsisplayScores } from './modules/Ui';
+import { fromPairs } from 'lodash';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const scoresArr = await APIhandel.getScores();
+  const { result } = scoresArr;
+  DsisplayScores(result);
+});
 
 const refresh = document.querySelector('.refresh');
 refresh.addEventListener('click', async () => {
   const scoresArr = await APIhandel.getScores();
   const { result } = scoresArr;
-  console.log(result);
   DsisplayScores(result);
+});
+
+const submit = document.querySelector('form');
+submit.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userName = document.getElementById('name').value;
+  const scoreValue = document.getElementById('score').value;
+  APIhandel.addScore(userName, scoreValue);
+  submit.reset();
 });
--- src/modules/Ui.js
+++ src/modules/Ui.js
@@ -1,10 +1,11 @@
-export default function DsisplayScores(scoresArr) {
+// eslint-disable-next-line import/prefer-default-export
+export const DsisplayScores = (scoresArr) => {
   const scoreList = document.querySelector('.board');
   scoreList.innerHTML = '';
   scoresArr.forEach((item) => {
     const scoreListItem = document.createElement('li');
     scoreListItem.className = 'list-item';
-    scoreListItem.innerHTML = `${item.user}:  ${item.score}`;
+    scoreListItem.innerHTML = `${item.user}:      ${item.score}`;
     scoreList.appendChild(scoreListItem);
   });
 }
